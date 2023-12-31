78c2b0a04033dea461b5f53b26866c937a7235ac
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2023-12-31 21:15:26 +0000

    Fix user/group params for sabnzbd

--- nixos/modules/services/networking/sabnzbd.nix
+++ nixos/modules/services/networking/sabnzbd.nix
@@ -26,7 +26,7 @@ in
 
       configFile = mkOption {
         type = types.path;
-        default = "/var/lib/sabnzbd/sabnzbd.ini";
+        default = "${cfg.dataDir}/sabnzbd.ini";
         description = lib.mdDoc "Path to config file.";
       };
 
@@ -41,6 +41,22 @@ in
         default = "sabnzbd";
         description = lib.mdDoc "Group to run the service as";
       };
+
+      dataDir = mkOption {
+        type = types.str;
+        default = "/var/lib/sabnzbd";
+        description = lib.mdDoc ''
+          The directory where sabnzbd stores its data files.
+        '';
+      };
+
+      openFirewall = mkOption {
+        type = types.bool;
+        default = false;
+        description = lib.mdDoc ''
+          Open ports in the firewall for the sabnzbd web interface
+        '';
+      };
     };
   };
 
@@ -48,17 +64,17 @@ in
   ###### implementation
 
   config = mkIf cfg.enable {
-
-    users.users.sabnzbd = {
-          uid = config.ids.uids.sabnzbd;
-          group = "sabnzbd";
-          description = "sabnzbd user";
-          home = "/var/lib/sabnzbd/";
-          createHome = true;
+    users.users = mkIf (cfg.user == "sabnzbd") {
+      sabnzbd = {
+        uid = config.ids.uids.sabnzbd;
+        group = cfg.group;
+        description = "sabnzbd user";
+        home = cfg.dataDir;
+      };
     };
 
-    users.groups.sabnzbd = {
-      gid = config.ids.gids.sabnzbd;
+    users.groups = mkIf (cfg.group == "sabnzbd") {
+      sabnzbd.gid = config.ids.gids.sabnzbd;
     };
 
     systemd.services.sabnzbd = {
@@ -68,10 +84,30 @@ in
         serviceConfig = {
           Type = "forking";
           GuessMainPID = "no";
-          User = "${cfg.user}";
-          Group = "${cfg.group}";
+          User = cfg.user;
+          Group = cfg.group;
+
+          # Run the pre-start script with full permissions (the "!" prefix) so it
+          # can create the data directory if necessary.
+          ExecStartPre = let
+            preStartScript = pkgs.writeScript "sabnzbd-run-prestart" ''
+              #!${pkgs.bash}/bin/bash
+
+              # Create data directory if it doesn't exist
+              if ! test -d "${cfg.dataDir}"; then
+                echo "Creating initial sabnzbd data directory in: ${cfg.dataDir}"
+                install -d -m 0700 -o "${cfg.user}" -g "${cfg.group}" "${cfg.dataDir}"
+              fi
+            '';
+          in
+            "!${preStartScript}";
+
           ExecStart = "${lib.getBin cfg.package}/bin/sabnzbd -d -f ${cfg.configFile}";
         };
+    };
+
+    networking.firewall = mkIf cfg.openFirewall {
+      allowedTCPPorts = [ 8080 ];
     };
   };
 }
